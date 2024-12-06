a95d0915757d2579d78bf8e7618e4fc252ff5f8d
Paweł Walkowiak <9573311u@qed.local>
2024-12-06 11:24:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wydarzeniewsudetach;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity{
+    private EditText email;
+    private EditText hasło;
+    private  TextView licznik;
+private Button przycisk;
